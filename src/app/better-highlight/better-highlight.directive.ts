d04b2b8748d80803cd1aa5c80263d6092c02af8a
lgants <lgants@gmail.com>
2017-07-30 00:23:01 -0500

    added event listener

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,7 +1,7 @@
 // custom directive
 // need to inform Angular that directive exists inside app.module.ts
-// better approach because angular also works in environments without the dom; so, basic highlight, which tries to change the dom, won't have desired effect 
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+// better approach because angular also works in environments without the dom; so, basic highlight, which tries to change the dom, won't have desired effect
+import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
 
 // will be recognized whenever adding the selector without square brackets to an element
 @Directive({
@@ -13,6 +13,15 @@ export class BetterHighlightDirective implements OnInit {
 
   // fourth argument, which is optional, is a flag
   ngOnInit(){
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue')
+  }
+
+  // triggered whenever the specified event occurs
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue')
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent')
   }
 }
